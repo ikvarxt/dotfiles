110637a8f54dbc9d7f0b007d9037515bafeb97f7
ikvarxt <leonekola@outlook.com>
2025-08-08 15:18:02 +0800

    feat: add yp in oil to copy relative path of current entry

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -12,6 +12,22 @@ return {
     view_options = {
       show_hidden = true,
     },
+    keymaps = {
+      ['yp'] = {
+        desc = 'Copy filepath to system clipboard',
+        callback = function()
+          local oil = require 'oil'
+          local dir = oil.get_current_dir()
+          local entry = oil.get_cursor_entry()
+          if not dir or not entry then
+            return
+          end
+          local s_cwd = vim.fn.getcwd():gsub('([%-%.%+%*%?%^%$%(%)%[%]%%])', '%%%1')
+          local relative_dir = dir:gsub(s_cwd .. '/', '')
+          vim.fn.setreg(vim.v.register, relative_dir .. entry.name)
+        end,
+      },
+    },
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
   },
   -- Optional dependencies
